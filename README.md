7cf2f8f8678b9832759f27815a783bcfe41652b8
Yaodong Li <mail@liyaodong.com>
2021-07-09 09:21:16 +0800

    Update readme and sample config

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # rss-hook
+
+## how it works
+
+1. download rss feed and parse
+2. get new items from rss and execute hook in config.js
+3. if the hook already done for the item, mark the key and store data to s3
+4. next time will skip items that already ran hook
+
+## Deploy
+
+`$ up`
+
+See more detail on apex up document
--- config.sample.js
+++ config.sample.js
@@ -1,11 +1,29 @@
+const axios = require("axios");
+const formatDate = require("date-fns/format");
+
+const feishuHookUrl = "yourFeishuHookUrl";
+
 const feedList = ["https://acloudguru.com/blog/tag/aws/feed"];
-const hook = (updatedNews) => {
-  // do what you want
-  // axios.post(hookURL, updatedNews) etc
+const hook = (item) => {
+  return Promise.all(
+    item.notifyItems.map((x) =>
+      axios.post(feishuHookUrl, generateFeishuContent(x))
+    )
+  );
 };
+const MAX_ITEMS_TO_SEARCH = 5;
+
+const s3 = {
+  region: "ap-south-1",
+  bucket: "rss-hook",
+};
+
+const generateFeishuContent = (content) => "YOUR HOOK BODY TEMPLATE";
 
 module.exports = {
   feedList,
   hook,
-  apiKey: "change to your api key",
+  apiKey: "SIMPLE API KEY FOR THIS APP",
+  s3,
+  MAX_ITEMS_TO_SEARCH,
 };
